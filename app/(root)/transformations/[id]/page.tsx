c19eb60c84aa6f7d532dc5f9033167a126b2ab56
Dev Tyagi <devutyagi2001@gmail.com>
2024-03-27 16:33:12 +0530

    initial commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TransformationsPage = () => {
+  return <div>TransformationsPage</div>;
+};
+
+export default TransformationsPage;
+
+// transformations wont happen on localhost:3000/transformations rather it will have theor own id
+// 3000/transformations/[id]--> dynamic routes concept
